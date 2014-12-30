69c6a222ad1a52979bf4634c17de0be22b5c2a92
ShuffleGTAO <sadek232323@gmail.com>
2014-12-30 15:47:07 +0100

    Scripts

---
+++ RandomVehicle/server.lua
@@ -0,0 +1,8 @@
+﻿function getRandomVehicle ()
+    los = {}
+        for k,v in ipairs ( getElementsByType ( vehicle ) ) do
+            table.insert ( los, v )
+        end
+        rnd = math.random ( #los )
+        return los[rnd]
+end
