9a638308acf66e9d24dbf2acbdf29a0a39a8548e
ShuffleGTAO <sadek232323@gmail.com>
2014-12-30 15:46:04 +0100

    Scripts

---
+++ PartNick/server.lua
@@ -0,0 +1,18 @@
+﻿function partNick ( plr, txt )
+	osoby = {}
+	gracz = txt and txt:gsub("#%x%x%x%x%x%x", ""):lower() or nil
+	for _,v in ipairs ( getElementsByType ( "player" ) ) do
+		gracz_ = getPlayerName(v):gsub("#%x%x%x%x%x%x", ""):lower()
+		if gracz_:find(gracz, 1, true) then
+			table.insert ( osoby, v )
+			name = v
+		end
+	end
+	if #osoby > 1 then
+		return outputChatBox ( "Znaleziono więcej niż 1 gracza!", plr )
+	end
+	if #osoby == 0 then
+		outputChatBox ( "Nie znaleziono żadnego gracza!", plr ) return false
+	end
+	return name
+end
---
+++ PlayTime/server.lua
@@ -0,0 +1,20 @@
+﻿function naliczCzas ()
+	for _,v in ipairs ( getElementsByType ( "player" ) ) do
+		konto = getPlayerAccount ( v )
+		h = getAccountData ( konto, "h" ) 
+		m = getAccountData ( konto, "m" )
+		s = getAccountData ( konto, "s" )
+		s = s +1
+		setAccountData ( konto, "s", s )
+		if s > 59 then
+			setAccountData ( konto, "s", 0 )
+			setAccountData ( konto, "m", m + 1 )
+		end
+		if m > 59 then
+			setAccountData ( konto, "m", 0 )
+			setAccountData ( konto, "h", h + 1 )
+		end
+	end
+end
+
+setTimer ( naliczCzas, 1000, 0 )
