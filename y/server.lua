51b7a7b1043c2570e4f0f07a5185da7dc1199dd6
ShuffleGTAO <sadek232323@gmail.com>
2014-12-31 17:01:54 +0100

    Scripts

--- y/server.lua
+++ y/server.lua
@@ -1,81 +1,70 @@
-﻿addCommandHandler ( "hol", function ( plr, cmd, id )
-	if id then
+﻿local function searchVehFromID ( id )
+	for _,v in ipairs ( getElementsByType ( "vehicle" ) ) do
+		if getElementData ( v, "id" ) == tonumber ( id ) then
+			return v
+		end
+	end
+	return false
+end
+
+local function isOwner ( auto, plr )
+	if getElementData ( auto, "owner" ) == getPlayerName ( plr ) then
+		return true
+	end
+	return false
+end
+
+
+local function sendOffer ( gracz, pojazd, plr )
+	name = getVehicleName ( pojazd )
+	id = getElementData ( pojazd, "id" ) or "Brak"
+	setElementData ( gracz, "Oferta", true )
+	setElementData ( gracz, "Pojazd", name )
+	setElementData ( gracz, "ID", id )
+	setElementData ( gracz, "Oferujacy", getPlayerName ( plr ) )
+	outputChatBox ( "Gracz "..getElementData ( gracz, "Oferujacy" ).." wyslał Ci ofertę sprzedaży pojazdu "..name.." ( ID "..id.." )", gracz, 255, 255, 255 )
+	outputChatBox ( "Wpisz /przyjmij, aby kupić. Masz 15 sek na reakcję.", gracz, 255, 255, 255 )
+	setTimer ( setElementData, 15000, 1, gracz, "Oferta", false )
+	setTimer ( setElementData, 15000, 1, gracz, "Pojazd", false )
+	setTimer ( setElementData, 15000, 1, gracz, "ID", false )
+	setTimer ( setElementData, 15000, 1, gracz, "Oferujacy", false )
+	setTimer ( outputChatBox, 15000, 1, "Oferta wygasła.", gracz, 255, 255, 255 )
+	setTimer ( outputChatBox, 15000, 1, "Oferta wygasła.", plr, 255, 255, 255 )
+end
+	
+	
+addCommandHandler ( "przyjmij", function ( plr, cmd )
+	if not getElementData ( plr, "Oferta" ) then 
+		return outputChatBox ( "Nie dostałeś oferty kupna pojazdu bądź oferta wygasłą", plr, 255, 255, 255 ) end
+	id = tonumber ( getElementData ( plr, "ID" ) )
+	name = getElementData ( plr, "Pojazd" )
+	oferujacy = getElementData ( plr, "Oferujacy" )
+	pojazd = searchVehFromID ( id )
+	if not pojazd then 
+		return outputChatBox ( "Nie znaleziono pojazdu o ID "..id.."", plr, 255, 255, 255 ) end
+	setElementData ( pojazd, "owner", getPlayerName ( plr ) )
+	outputChatBox ( "Kupiłeś pojazd "..name.." ( ID "..id.." ) od gracza "..oferujacy.."", plr, 0, 255, 0 )
+	outputChatBox ( "Gracz "..getPlayerName ( plr ).." przyjął ofertę kupna pojazdu!", getPlayerFromName ( oferujacy ), 0, 255, 0 )
+	setElementData ( plr, "Oferta", false )
+	setElementData ( plr, "Pojazd", false )
+	setElementData ( plr, "ID", false )
+	setElementData ( plr, "Oferujacy", false )
+end)
+	
+	
+
+addCommandHandler ( "v.sprzedaj", function ( plr, cmd, id, gracz )
+	if id and gracz then
 		id = tonumber ( id )
-		for _,v in ipairs ( getElementsByType ( "vehicle" ) ) do
-			if getElementData ( v, "id" ) then
-				if getElementData ( v, "id" ) == id then
-					pojazd = v
-				end
-			end
-		end
-		id2 = getElementData ( pojazd, "id" ) or "Brak"
-		owner = getElementData ( pojazd, "owner" ) or "Brak"
-		name = getVehicleName ( pojazd )
-		c1,c2,c3,c4 = getVehicleColor ( pojazd )
-		xmlSave = xmlLoadFile ( "zapis.xml" )
-		child = xmlCreateChild ( xmlSave, "Pojazd" )
-		xmlNodeSetAttribute ( child, "ID", id2 )
-		xmlNodeSetAttribute ( child, "owner", owner )
-		xmlNodeSetAttribute ( child, "name", name )
-		xmlNodeSetAttribute ( child, "c1", c1 )
-		xmlNodeSetAttribute ( child, "c2", c2 )
-		xmlNodeSetAttribute ( child, "c3", c3 )
-		xmlNodeSetAttribute ( child, "c4", c4 )
-		destroyElement ( pojazd )
-		outputChatBox ( "Pojazd "..name.." ( ID "..id2.." ) został odholowany do przechowalni!", plr, 255, 255, 255 )
-		xmlSaveFile ( xmlSave )
-		xmlUnloadFile ( xmlSave )
+		auto = searchVehFromID ( id )
+		if not auto then
+			return outputChatBox ( "Nie znaleziono pojazdu o ID "..id.."", plr ) end
+		if not isOwner ( auto, plr ) then
+			return outputChatBox ( "Pojazd "..getVehicleName ( auto ).." nie jest Twój", plr ) end
+		kupujacy = getPlayerFromName ( gracz )
+		if not kupujacy then
+			return outputChatBox ( "Gracz "..gracz.." nie jest online!", plr ) end
+		sendOffer ( kupujacy, auto, plr )
 	end
 end)
-
-
-addCommandHandler ( "lista", function ( plr )
-	poj = {}
-	xmlLoad = xmlLoadFile ( "zapis.xml" )
-	for _,v in ipairs ( xmlNodeGetChildren ( xmlLoad ) ) do
-		if xmlNodeGetAttribute ( v, "owner" ) == getPlayerName ( plr ) then
-			name = xmlNodeGetAttribute ( v, "name" )
-			id = xmlNodeGetAttribute ( v, "ID" )
-			i = ""..name.." ( ID "..id.." ) "
-			table.insert ( poj, i )
-		end
-	end
-	if #poj == 0 then
-		outputChatBox ( "Nie posiadasz żadnego pojazdu w przechowywalni.", plr, 255, 255, 255 )
-	elseif #poj > 0 then
-		msg = table.concat ( poj, ", " )
-		outputChatBox ( "Pojazdy :", plr, 255, 255, 255 )
-		outputChatBox ( msg, plr, 255, 255, 255 )
-	end
-	xmlUnloadFile ( xmlLoad )
-end)
-
-
-
-addCommandHandler ( "odkup", function ( plr, cmd, id3 )
-	if id3 then
-		pojazd_number = {}
-		id3 = tonumber ( id3 )
-		xmlUnload = xmlLoadFile ( "zapis.xml" )
-		for _,v in ipairs ( xmlNodeGetChildren ( xmlUnload ) ) do
-			if tonumber ( xmlNodeGetAttribute ( v, "ID" ) ) == id3 then
-				name = xmlNodeGetAttribute ( v, "name" )
-				table.insert ( pojazd_number, name )
-				owner = xmlNodeGetAttribute ( v, "owner" )
-				c1,c2,c3,c4 = xmlNodeGetAttribute ( v, "c1" ), xmlNodeGetAttribute ( v, "c2" ), xmlNodeGetAttribute ( v, "c3" ), xmlNodeGetAttribute ( v, "c4" )
-				node = v
-			end
-		end
-		if #pojazd_number == 0 then
-			return outputChatBox ( "Nie znaleziono pojazdu w przechowywalni o ID "..id3..".", plr, 255, 255, 255 ) end
-		auto = createVehicle ( getVehicleIDFromName ( name ), -276.4560546875, 1056.4609375, 19.589454650879 )
-		setVehicleColor ( auto, c1,c2,c3,c4 )
-		setElementData ( auto, "owner", owner )
-		setElementData ( auto, "id", id3 )
-		outputChatBox ( "Pojazd "..name.." ( ID "..id3.." ) został wyciągnięty z przechowywalni", plr, 255, 255, 255 )
-		setElementData ( auto, "message", "Właściciel : "..owner.."" )
-		xmlDestroyNode ( node )
-		xmlSaveFile ( xmlUnload )
-		xmlUnloadFile ( xmlUnload )
-	end
-end)
+			
