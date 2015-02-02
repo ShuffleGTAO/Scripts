04b51c5e816b31eaa02fa6676775ce225d0cff4d
ShuffleGTAO <sadek232323@gmail.com>
2015-02-02 16:06:04 +0100

    Scripts

---
+++ fr-radary/FR_Radary.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : OwnPlay
+Source Server Version : 50541
+Source Host           : sql-waw1.ServerProject.pl:3306
+Source Database       : db_9591
+
+Target Server Type    : MYSQL
+Target Server Version : 50541
+File Encoding         : 65001
+
+Date: 2015-02-02 16:04:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `FR_Radary`
+-- ----------------------------
+DROP TABLE IF EXISTS `FR_Radary`;
+CREATE TABLE `FR_Radary` (
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `z` int(11) DEFAULT NULL,
+  `predkosc` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of FR_Radary
+-- ----------------------------
---
+++ fr-radary/server.lua
@@ -0,0 +1,63 @@
+﻿local dbname = "db_9591"
+local host = "sql-waw1.ServerProject.pl"
+local username = "db_9591"
+local pass = "aeb09ff1464d"
+
+liczba = 0
+
+local db = dbConnect("mysql", "dbname="..dbname..";host="..host.."", ""..username.."",""..pass.."","share=1")
+
+addEventHandler ( "onResourceStart", resourceRoot, function ()
+	local q = dbQuery ( db, "SELECT * FROM FR_Radary" )
+	local w = dbPoll ( q, -1 )
+	dbFree ( q )
+	for k,v in ipairs ( w ) do
+		marker = createMarker ( v.x, v.y, v.z-1, "cylinder", 6, 255, 255, 255 )
+		setElementData ( marker, "predkosc", v.predkosc )
+		liczba = k
+	end
+	outputDebugString ( "[Radary] Utworzono "..liczba.." radarów" )
+end)
+
+local function getVehicleSpeed ( element )
+	speedx, speedy, speedz = getElementVelocity ( element )
+	actualspeed = (speedx^2 + speedy^2 + speedz^2)^(0.5) 
+	kmh = actualspeed * 180
+	return kmh
+end
+
+addEventHandler ( "onMarkerHit", root, function ( element )
+	if getElementData ( source, "predkosc" ) then
+		speed = tonumber ( getElementData ( source, "predkosc" ) )
+		if getElementType ( element ) == "player" then
+			veh = getPedOccupiedVehicle ( element )
+			if veh then
+				if getVehicleSpeed ( veh ) > speed then
+					local przekroczono = math.floor ( getVehicleSpeed ( veh ) - speed )
+					local kara = string.format ( "%3.0f", przekroczono /3 )
+					for i = 1, 10 do
+						outputChatBox ( " ", element )
+					end
+					outputChatBox ( "Przekroczyłeś prędkość o "..przekroczono.." km/h! Dopuszczalna prędkość : "..speed.." km/h", element, 255, 255, 255 )
+					outputChatBox ( "Zapłaciłeś : "..kara.." $ kary.", element, 255, 255, 255 )
+				end
+			end
+		end
+	end
+end)
+
+
+
+addCommandHandler ( "dodaj.radar", function ( plr, cmd, predkosc )
+	if isObjectInACLGroup ( "user."..getAccountName ( getPlayerAccount ( plr ) ), aclGetGroup ( "Admin" ) ) then
+		if type(tonumber(predkosc)) ~= "number" then
+			if not predkosc or predkosc == nil or predkosc < 0 then
+				return outputChatBox ( "[Użycie] /dodaj.radar <prędkość>", plr, 255, 255, 255 )
+			end
+		end
+		local x,y,z = getElementPosition ( plr )
+		dbExec ( db, "INSERT INTO FR_Radary ( x, y, z, predkosc ) VALUES ( ?,?,?,? )", x, y, z, tonumber(predkosc) )
+		outputChatBox ( "Dodano w tym miejscu radar z ograniczoną prędkością do "..predkosc.." km/h", plr, 255, 255, 255 )
+		restartResource ( getResourceFromName ( "fr-radary" ) )
+	end
+end)
