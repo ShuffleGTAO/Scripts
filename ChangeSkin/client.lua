5e13d4dcb1df27f2342aaff8b66f43007f3a85cb
ShuffleGTAO <sadek232323@gmail.com>
2015-02-15 13:52:19 +0100

    Scripts

---
+++ ChangeSkin/client.lua
@@ -0,0 +1,115 @@
+﻿arrow = {}
+valid = {}
+
+
+
+
+--USTAWIENIA
+
+CHAT = true
+HUD = false
+MOUSE = true
+
+--USTAWIENIA
+
+mojeW,mojeH = 640, 480
+sW,sH = guiGetScreenSize()
+w, h = (sW/mojeW), (sH/mojeH)
+
+arrow[1] = guiCreateStaticImage(233*w, 307*h, 57*w, 43*h, "left_off.png", false)
+
+
+arrow[2] = guiCreateStaticImage(346*w, 307*h, 57*w, 43*h, "right_off.png", false)
+
+addEventHandler ( "onClientResourceStart", resourceRoot, function ()
+	for k,_ in ipairs ( arrow ) do
+		guiSetVisible ( arrow[k], true )
+	end
+	if MOUSE == true then
+		showCursor ( true )
+	end
+	if HUD == false then
+		showPlayerHudComponent ( "all", false )
+	else
+		showPlayerHudComponent ( "all", true )
+	end
+	if CHAT == true then
+		showChat ( true )
+	else
+		showChat ( false )
+	end
+	for _,v in ipairs ( getValidPedModels() ) do
+		table.insert ( valid, v )
+	end
+	setElementData ( arrow[1], "right", true )
+	setElementData ( arrow[1], "left", true )
+	ped = createPed ( 0, -693.64014, 919.16327, 12.26443, 90 )
+	setCameraMatrix ( -695.71442, 916.42206, 12.28372, -693.64014, 919.16327, 12.26443 )
+	bindKey ( "ENTER", "DOWN", acceptSkin )
+end)
+
+
+addEventHandler ( "onClientMouseEnter", root, function ()
+	if getElementData ( source, "left" ) then
+		guiSetVisible ( arrow[1], false )
+		arrow[1] = guiCreateStaticImage(233*w, 307*h, 57*w, 43*h, "left_on.png", false)
+		setElementData ( arrow[1], "left", true )
+	end
+	if getElementData ( source, "right" ) then
+		guiSetVisible ( arrow[2], false )
+		arrow[2] = guiCreateStaticImage(346*w, 307*h, 57*w, 43*h, "right_on.png", false)
+		setElementData ( arrow[2], "right", true )
+	end
+end)
+
+addEventHandler ( "onClientMouseLeave", root, function ()
+	if getElementData ( source, "left" ) then
+		guiSetVisible ( arrow[1], false )
+		arrow[1] = guiCreateStaticImage(233*w, 307*h, 57*w, 43*h, "left_off.png", false)
+		setElementData ( arrow[1], "left", true )
+	end
+	if getElementData ( source, "right" ) then
+		guiSetVisible ( arrow[2], false )
+		arrow[2] = guiCreateStaticImage(346*w, 307*h, 57*w, 43*h, "right_off.png", false)
+		setElementData ( arrow[2], "right", true )
+	end
+end)
+
+count = 0
+
+
+addEventHandler ( "onClientGUIClick", root, function ()
+	if getElementData ( source, "right" ) then
+		if count == #valid then
+			return
+		end
+		count = count + 1
+		local skin = tonumber ( valid[count] )
+		setElementModel ( ped, skin )
+	end
+	if getElementData ( source, "left" ) then
+		if count == 0 then
+			return
+		end
+		count = count - 1
+		local skin = tonumber ( valid[count] )
+		setElementModel ( ped, skin )
+	end
+end)
+
+
+function acceptSkin ()
+	setElementModel ( localPlayer, getElementModel ( ped ) )
+	destroyElement ( ped )
+	fadeCamera ( false )
+	setTimer ( fadeCamera, 2000, 1, true )
+	showCursor ( false )
+	showChat ( true )
+	showPlayerHudComponent ( "all", true )
+	setCameraTarget ( localPlayer )
+	for k,_ in ipairs ( arrow ) do
+		guiSetVisible ( arrow[k], false )
+	end
+	outputChatBox ( "Shuffle życzy miłej gry!", 255, 255, 255 )
+end
+	
---
+++ hud/client.lua
@@ -0,0 +1,51 @@
+﻿local screenWidth, screenHeight = guiGetScreenSize()
+local w = screenWidth/640
+local y = screenHeight/480
+
+local opened = {}
+
+opened.stat = false
+
+
+addEventHandler ( "onClientRender", root, function ()
+	if opened.stat == true then
+		local time = getRealTime ()
+		if time.hour < 10 then
+			time.hour = "0"..time.hour
+		end
+		if time.minute < 10 then
+			time.minute = "0"..time.minute
+		end
+		local czas = ""..time.hour.." : "..time.minute..""
+		local playerHealth = math.floor( getElementHealth( localPlayer ))
+		local playerArmor = math.floor( getPedArmor( localPlayer ))
+		local playerCash = string.format ( "$ %08.0f", getPlayerMoney ( localPlayer ))
+		dxDrawText(czas, 481*w, 24*y, 609*w, 57*y, tocolor(0, 0, 0, 255), 1.00*w, "pricedown", "center", "center", false, false, true, false, false)
+		dxDrawText(czas, 480*w, 23*y, 608*w, 56*y, tocolor(255, 255, 255, 255), 1.00*w, "pricedown", "center", "center", false, false, true, false, false)
+		dxDrawRectangle(468*w, 72*y, 151*w, 10*y, tocolor(0, 0, 0, 255), true)
+		dxDrawRectangle(474*w, 72*y, 140*w/100*playerHealth, 10*y, tocolor(163, 0, 0, 255), true)
+		--dxDrawText("HEALTH : "..playerHealth.." %", 516*w, 73*y, 572*w, 84*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("HEALTH : "..playerHealth.." %", 516*w, 71*y, 572*w, 82*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("HEALTH : "..playerHealth.." %", 514*w, 73*y, 570*w, 84*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("HEALTH : "..playerHealth.." %", 514*w, 71*y, 570*w, 82*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		dxDrawText("HEALTH : "..playerHealth.." %", 515*w, 72*y, 571*w, 83*y, tocolor(255, 255, 255, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		dxDrawRectangle(468*w, 98*y, 151*w, 10*y, tocolor(0, 0, 0, 255), true)
+		dxDrawRectangle(474*w, 98*y, 140*w/100*playerArmor, 10*y, tocolor(85, 85, 85, 255), true)
+		--dxDrawText("ARMOR : "..playerArmor.." %", 516*w, 99*y, 572*w, 110*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("ARMOR : "..playerArmor.." %", 516*w, 97*y, 572*w, 108*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("ARMOR : "..playerArmor.." %", 514*w, 99*y, 570*w, 110*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		--dxDrawText("ARMOR : "..playerArmor.." %", 514*w, 97*y, 570*w, 108*y, tocolor(0, 0, 0, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		dxDrawText("ARMOR : "..playerArmor.." %", 515*w, 98*y, 571*w, 109*y, tocolor(255, 255, 255, 255), 0.60*w, "default-bold", "center", "center", false, false, true, false, false)
+		dxDrawText(playerCash, 469*w, 133*y, 615*w, 155*y, tocolor(0, 0, 0, 255), 1.00*w, "sans", "center", "center", false, false, true, false, false)
+		dxDrawText(playerCash, 468*w, 132*y, 614*w, 154*y, tocolor(7, 67, 4, 255), 1.00*w, "sans", "center", "center", false, false, true, false, false)
+	end
+end)
+
+addEventHandler ( "onClientResourceStart", resourceRoot, function ()
+	showPlayerHudComponent ( "clock", false )
+	showPlayerHudComponent ( "armour", false )
+	showPlayerHudComponent ( "health", false )
+	showPlayerHudComponent ( "weapon", false )
+	showPlayerHudComponent ( "money", false )
+	opened.stat = true
+end)
