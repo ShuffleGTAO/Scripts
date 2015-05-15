041e5ddfae95d29f5418f88b3fd9f5fd266929bd
ShuffleGTAO <sadek232323@gmail.com>
2015-05-15 13:10:53 +0200

    Scripts

---
+++ Wypozyczalnia/client.lua
@@ -0,0 +1,34 @@
+﻿mojeW,mojeH = 640, 480
+sW,sH = guiGetScreenSize()
+width, height = (sW/mojeW), (sH/mojeH)
+
+addEventHandler("onClientRender",root,function()
+	local x, y, z = getCameraMatrix( )
+	local dimension = getElementDimension( localPlayer )
+	for key, vehicle in ipairs ( getElementsByType( "vehicle" ) ) do
+		if getElementDimension( vehicle ) == dimension then
+			local px, py, pz = getElementPosition( vehicle )
+			local distance = getDistanceBetweenPoints3D( px, py, pz, x, y, z )
+			if distance <= 20 then
+				local text = getElementData( vehicle, "WypozyczalniaOpis" )
+				if text and ( distance < 2 or isLineOfSightClear( x, y, z, px, py, pz + 1.1, true, false, false, true, false, true, true ) ) then
+					local sx, sy = getScreenFromWorldPosition( px, py, pz+0.2 )
+					if sx and sy then
+						local w = dxGetTextWidth( tostring( text ) )
+						local h = ( text and 2 or 1 ) * dxGetFontHeight( )
+						local sin=math.sin(getTickCount()/400)*70
+						local cos=math.sin(getTickCount()/400)*0.1
+						dxDrawText( tostring( text ), sx+3, sy+30+3, sx, sy, tocolor( 0, 0, 0, 70+sin ), (1.3*width/2.5)+cos, "default", "center", "center" )
+						dxDrawText( tostring( text ), sx, sy+30, sx, sy, tocolor( 255, 255, 255, 70+sin ), (1.3*width/2.5)+cos, "default", "center", "center",_,_,_,true )
+					end
+				end
+			end
+		end
+	end	
+end)
+
+
+
+
+
+
---
+++ Wypozyczalnia/server.lua
@@ -0,0 +1,53 @@
+﻿bike={}
+
+
+
+local bikes={
+	--x,y,z,rotx,roty,rotz
+	{-302.9248046875, 1082.025390625, 19.7421875,0,0,0},
+	{-299.955078125, 1081.7333984375, 19.7421875,0,0,0},
+	{-294.62109375, 1081.708984375, 19.734390258789,0,0,0},
+	{-297.23828125, 1086.8984375, 19.734390258789,0,0,0}
+}
+
+
+addEventHandler("onResourceStart",resourceRoot,function()
+	for count,v in ipairs(bikes)do
+		id=count
+		bike[id]=createVehicle(481,v[1],v[2],v[3],v[4],v[5],v[6])
+		setElementData(bike[id],"WypozyczalniaOpis","Wypożyczalnia rowerów\nRower ID : "..id)
+	end
+end)
+
+timer={}
+
+
+addEventHandler("onPlayerVehicleExit",root,function(veh,seat)
+	if seat==0 then
+		timer[veh]={}
+		for i=1,#bikes do
+			if veh==bike[i] then
+				outputChatBox("*Wyszedłeś(aś) z roweru należącego do wypożyczalni. Masz 5 sekund żeby na niego wrócić inaczej zostanie zrespiony na miejsce zaparkowania i zostanie pobrana opłata w wysokości 5 $",source,255,255,255)
+				timer[veh][1],timer[veh][2]=setTimer(respawnVehicle,5000,1,veh)
+				timer[veh][2]=setTimer(takePlayerMoney,5000,1,source,5)
+			end
+		end
+	end
+end)
+
+
+addEventHandler("onPlayerVehicleEnter",root,function(veh,seat)
+	if seat==0 then
+		for i=1,#bikes do
+			if veh==bike[i] then
+				for k=1,2 do
+					if timer[veh] then
+						if isTimer(timer[veh][k]) then
+							killTimer(timer[veh][k])
+						end
+					end
+				end
+			end
+		end
+	end
+end)
